c295e285cec13b0ef24920f789da45e5939364ac
North <north.ap@live.com>
2016-09-01 00:53:06 +0700

    initial commit

---
+++ add_image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  add_image
+//
+//  Created by North on 9/1/2559 BE.
+//  Copyright © 2559 North. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
